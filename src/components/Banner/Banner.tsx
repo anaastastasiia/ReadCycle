f9672e11ac09faa6c13e539af44b67ccfe1b7aa5
anaastastasiia <nastiaproturenko5@gmail.com>
2024-11-14 16:18:10 +0100

    sales section

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -6,64 +6,97 @@ interface BannerProps {
   title: string;
   subtitle: string;
   link: string;
-  tag: string;
-  reverse: boolean;
+  author: string;
+  reverse?: boolean;
+  price: number;
+  discount?: number;
 }
 
-const Banner = ({ image, title, subtitle, tag, reverse }: BannerProps) => {
+const Banner = ({
+  image,
+  title,
+  subtitle,
+  author,
+  reverse,
+  price,
+  discount,
+}: BannerProps) => {
+  const getPriceWithDiscount = (price: number, discount?: number): string => {
+    if (discount) {
+      return (price * (1 - discount / 100)).toFixed(2);
+    }
+    return "";
+  };
+
   return (
-    <div className="bg-[#f9f9f9] pb-14">
-      <div className="container">
-        <div className="grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0">
-          <div
-            className={`flex justify-start items-center ${
-              reverse && "md:order-last md:justify-end"
-            }`}
-          >
-            <motion.img
-              initial={{ opacity: 0, scale: 0.5 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
-              src={image}
-              alt=""
-              className="w-[400px] h-full object-cover"
-            />
-          </div>
-          <div className="flex flex-col justify-center text-center md:text-left space-y-4 lg:max-w-[500px]">
-            <motion.p
-              variants={SlideUp(0.5)}
-              initial="hidden"
-              whileInView={"visible"}
-              className="text-sm text-orange-600 font-semibold capitalize"
-            >
-              {tag}
-            </motion.p>
-            <motion.p
-              variants={SlideUp(0.7)}
-              initial="hidden"
-              whileInView={"visible"}
-              className="text-xl lg:text-2xl capitalize font-semibold"
-            >
-              {title}
-            </motion.p>
-            <motion.p
-              variants={SlideUp(0.9)}
-              initial="hidden"
-              whileInView={"visible"}
-              className="text-sm text-slate-500"
-            >
-              {subtitle}
-            </motion.p>
-            <motion.div
-              variants={SlideUp(1.1)}
-              initial="hidden"
-              whileInView={"visible"}
-              className="flex justify-center md:justify-start"
-            >
-              <button className="primary-btn !mt-5">Get Started</button>
-            </motion.div>
-          </div>
+    <div
+      className={`flex justify-start items-center max-w-1/2 max-h-[300px] content-start py-6 relative ${
+        reverse && "md:order-last md:justify-end"
+      }`}
+    >
+      <motion.img
+        initial={{ opacity: 0, scale: 0.5 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+        src={image}
+        alt=""
+        className="w-full h-full object-cover"
+      />
+      {discount ? (
+        <div className="absolute top-2 -left-2  bg-red-500 text-white md:text-lg  font-bold rounded-full px-2 py-1">
+          -{discount}%
         </div>
+      ) : null}
+      <div className="flex flex-col justify-center text-center md:text-left space-y-2 lg:max-w-1/2 px-6">
+        <motion.p
+          variants={SlideUp(0.5)}
+          initial="hidden"
+          whileInView={"visible"}
+          className="text-sm text-orange-600 font-semibold capitalize"
+        >
+          {author}
+        </motion.p>
+        <motion.p
+          variants={SlideUp(0.7)}
+          initial="hidden"
+          whileInView={"visible"}
+          className="text-xl lg:text-2xl capitalize font-semibold"
+        >
+          {title}
+        </motion.p>
+        <motion.p
+          variants={SlideUp(0.9)}
+          initial="hidden"
+          whileInView={"visible"}
+          className="text-sm text-slate-500 overflow-hidden line-clamp-3"
+        >
+          {subtitle} <button>Show more</button>
+        </motion.p>
+        <motion.p
+          variants={SlideUp(1.0)}
+          initial="hidden"
+          whileInView={"visible"}
+          className="text-xl lg:text-2xl capitalize font-semibold"
+        >
+          {discount ? (
+            <div>
+              <span className="text-red-700">
+                {getPriceWithDiscount(price, discount)} PLN{" "}
+              </span>
+              <span className="line-through">{price.toFixed(2)} PLN</span>
+            </div>
+          ) : (
+            <span>{price.toFixed(2)} PLN</span>
+          )}
+        </motion.p>
+        <motion.div
+          variants={SlideUp(1.1)}
+          initial="hidden"
+          whileInView={"visible"}
+          className="flex justify-center md:justify-start"
+        >
+          <button className="primary-btn">Add To Cart</button>
+        </motion.div>
       </div>
     </div>
   );
---
+++ src/components/Sales/Sales.tsx
@@ -0,0 +1,41 @@
+import Banner from "../Banner/Banner";
+import Img1 from "../../assets/courtOfThornsAndRoses.jpg";
+import Img2 from "../../assets/theMountainIsYou.jpg";
+
+const BannerData = {
+  image: Img1,
+  author: "Maas Sarah J.",
+  title: "A Court of Thorns And Roses",
+  subtitle:
+    "Feyre is a huntress. And when she sees a deer in the forest being pursued by a wolf, she kills the predator and takes its prey to feed herself and her family.",
+  link: "#",
+  price: 76,
+  discount: 20,
+};
+
+const BannerData2 = {
+  image: Img2,
+  author: "Brianna Wiest",
+  title: "The Mountain Is You",
+  subtitle:
+    "This is a book about self-sabotage. Why we do it, when we do it, and how to stop doing it—for good.Coexisting but conflicting needs create self-sabotaging behaviors. This is why we resist efforts to change, often until they feel completely futile. ",
+  link: "#",
+  price: 34,
+};
+
+const Sales = () => {
+  return (
+    <div className="bg-[#f9f9f9] pb-14">
+      <div className="container">
+        <div className="grid lg:grid-cols-2 space-y-6 md:space-y-0 ">
+          <Banner {...BannerData} />
+          <Banner {...BannerData2} reverse />
+          <Banner {...BannerData2} />
+          <Banner {...BannerData2} reverse />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sales;
