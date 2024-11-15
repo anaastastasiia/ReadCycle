c4afdbeda32cd64d7b9e84785bd32b091e8a93cc
anaastastasiia <nastiaproturenko5@gmail.com>
2024-11-15 11:34:41 +0100

    fix book width and add title for sales section

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -30,23 +30,24 @@ const Banner = ({
 
   return (
     <div
-      className={`flex justify-start items-center max-w-1/2 max-h-[300px] content-start py-6 relative ${
+      className={`flex justify-start items-center max-w-1/2 md:max-h-[300px] content-start py-6 relative gap-2 flex-col sm:flex-row  ${
         reverse && "md:order-last md:justify-end"
       }`}
     >
-      <motion.img
+      <motion.div
         initial={{ opacity: 0, scale: 0.5 }}
         whileInView={{ opacity: 1, scale: 1 }}
         transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
-        src={image}
-        alt=""
-        className="w-full h-full object-cover"
-      />
-      {discount ? (
-        <div className="absolute top-2 -left-2  bg-red-500 text-white md:text-lg  font-bold rounded-full px-2 py-1">
-          -{discount}%
-        </div>
-      ) : null}
+        className="min-w-[200px] min-h-[300px] max-h-[300px] max-w-[300px] bg-gray-500 flex justify-center items-center relative overflow-hidden"
+      >
+        <motion.img src={image} alt="" className="w-full h-full object-cover" />
+        {discount ? (
+          <div className="absolute top-0 left-0  bg-red-500 text-white md:text-lg  font-bold rounded-full px-2 py-1">
+            -{discount}%
+          </div>
+        ) : null}
+      </motion.div>
+
       <div className="flex flex-col justify-center text-center md:text-left space-y-2 lg:max-w-1/2 px-6">
         <motion.p
           variants={SlideUp(0.5)}
@@ -76,7 +77,7 @@ const Banner = ({
           variants={SlideUp(1.0)}
           initial="hidden"
           whileInView={"visible"}
-          className="text-xl lg:text-2xl capitalize font-semibold"
+          className="text-2xl capitalize font-semibold"
         >
           {discount ? (
             <div>
--- src/components/Sales/Sales.tsx
+++ src/components/Sales/Sales.tsx
@@ -21,13 +21,20 @@ const BannerData2 = {
     "This is a book about self-sabotage. Why we do it, when we do it, and how to stop doing it—for good.Coexisting but conflicting needs create self-sabotaging behaviors. This is why we resist efforts to change, often until they feel completely futile. ",
   link: "#",
   price: 34,
+  discount: 30,
 };
 
 const Sales = () => {
   return (
     <div className="bg-[#f9f9f9] pb-14">
       <div className="container">
-        <div className="grid lg:grid-cols-2 space-y-6 md:space-y-0 ">
+        <div className="space-y-4 px-6 pb-6 text-center max-w-[700px] mx-auto mb-5">
+          <p className="font-semibold text-3xl ">
+            Discover Great Deals:{" "}
+            <span className="text-red-600">Books on Sale!</span>
+          </p>
+        </div>
+        <div className="grid lg:grid-cols-2 space-y-6 md:space-y-0 gap-6">
           <Banner {...BannerData} />
           <Banner {...BannerData2} reverse />
           <Banner {...BannerData2} />
