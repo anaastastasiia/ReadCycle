6591b0ec52073c4cff9cc7b81d8b08045b697587
anaastastasiia <nastiaproturenko5@gmail.com>
2024-11-18 20:57:00 +0100

    filter books by categories

--- back-end/models/book.js
+++ back-end/models/book.js
@@ -55,4 +55,12 @@ const bookSchema = mongoose.Schema({
     }
 })
 
+bookSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+})
+
+bookSchema.set('toJSON', {
+    virtuals: true
+})
+
 exports.Book = mongoose.model('Book', bookSchema);
