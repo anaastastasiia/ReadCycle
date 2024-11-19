fc0b7f9ea5663205e714c128cbf0725d3f2271d9
anaastastasiia <nastiaproturenko5@gmail.com>
2024-11-19 10:12:53 +0100

    create user and hash his password

--- back-end/routers/users.js
+++ back-end/routers/users.js
@@ -1,6 +1,7 @@
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 router.get('/', async (req, res) => {
     const userList = await User.find();
@@ -9,6 +10,29 @@ router.get('/', async (req, res) => {
         res.status(500).json({success: false})
     }
     res.send(userList);
+})
+
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        isAdmin: req.body.isAdmin
+    })
+
+    user = await user.save();
+
+    if (!user) {
+        return res.status(404).send('User cannot be created');
+    } 
+
+    res.send(user);
 })
 
 module.exports = router;
