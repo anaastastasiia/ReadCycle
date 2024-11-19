98fd3530da4def7e334888a2c15df043dc6d77fa
anaastastasiia <nastiaproturenko5@gmail.com>
2024-11-19 11:31:18 +0100

    user authentication

--- back-end/routers/users.js
+++ back-end/routers/users.js
@@ -46,4 +46,19 @@ router.post('/', async (req, res) => {
     res.send(user);
 })
 
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if (!user) {
+        return res.status(400).send('The user not found')
+    }
+
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        return res.status(200).send('User Authenticated')
+    } else {
+        return res.status(400).send('Password is wrong')
+    }
+
+})
+
 module.exports = router;
