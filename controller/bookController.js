afed532013f8b6d7217d5006bf9cb2d474db5e7b
anaastastasiia <nastiaproturenko5@gmail.com>
2025-01-19 16:49:02 +0100

    initial commit

---
+++ controller/bookController.js
@@ -0,0 +1,13 @@
+// class BookController {
+//     async createBook(req, res) {
+//         const { name, author } = req.body;
+//         console.log(name, author);
+//         res.json('ok');
+//     }
+//     async getBooks(req, res) {}
+//     async getBookById(req, res) {}
+//     async updateBook(req, res) {}
+//     async deleteBook(req, res) {}
+// }
+
+// module.exports = new BookController();
---
+++ routes/bookRoute.js
@@ -0,0 +1,11 @@
+// const Router = require('express');
+// const router = new Router();
+// const bookController = require('../controller/bookController');
+
+// router.post('/book', bookController.createBook);
+// router.get('/book', bookController.getBooks);
+// router.get('/book/:id', bookController.getBookById);
+// router.put('/book', bookController.updateBook);
+// router.delete('/book/:id', bookController.deleteBook);
+
+// module.exports = router;
