f3451ad5e4bcfb33cf2f239c2a5ea246a13a6a63
anaastastasiia <nastiaproturenko5@gmail.com>
2025-03-11 17:59:02 +0100

    base filters

--- front-end/src/pages/BookDetailsPage.tsx
+++ front-end/src/pages/BookDetailsPage.tsx
@@ -1,12 +1,18 @@
 import { Book } from '../components/Book/Book';
+import { BookFilters } from '../components/BookFilters/BookFilters';
 import { booksStore } from '../store/useBooks';
 
 export const BookDetailsPage = () => {
     const { bookDetails } = booksStore();
 
     return (
-        <div className="overflow-x-hidden m-0">
-            {bookDetails ? <Book {...bookDetails} /> : null}
+        <div className="overflow-x-hidden m-0 flex container">
+            <div className="w-1/5 p-4 min-h-screen">
+                <BookFilters />
+            </div>
+            <div className="w-4/5 p-6">
+                {bookDetails ? <Book {...bookDetails} /> : null}
+            </div>
         </div>
     );
 };
