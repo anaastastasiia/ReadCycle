78d924c516273f4d4be8af7a74c93975f8428f63
anaastastasiia <nastiaproturenko5@gmail.com>
2025-04-30 12:41:42 +0200

    new buttos for BookCard

--- front-end/src/pages/User/UserBooksPage.tsx
+++ front-end/src/pages/User/UserBooksPage.tsx
@@ -15,10 +15,19 @@ export const UserBooksPage = () => {
     }, []);
 
     return (
-        <div className="max-w-6xl mx-auto p-4">
+        <div className="overflow-x-hidden flex container flex-col my-8">
             <h1 className="text-2xl font-bold mb-4">Moї книги на продаж</h1>
 
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {userSellBooks.map((book) => (
+                    <BookCard key={book.id} book={book} />
+                ))}
+                {userSellBooks.map((book) => (
+                    <BookCard key={book.id} book={book} />
+                ))}
+                {userSellBooks.map((book) => (
+                    <BookCard key={book.id} book={book} />
+                ))}
                 {userSellBooks.map((book) => (
                     <BookCard key={book.id} book={book} />
                 ))}
