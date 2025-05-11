a80befff2387a4f9a684119026566547214b64bb
anaastastasiia <nastiaproturenko5@gmail.com>
2025-05-11 13:48:03 +0200

    get all books from db

--- back-end/src/routes/bookRoute.js
+++ back-end/src/routes/bookRoute.js
@@ -86,6 +86,76 @@ router.get('/onSale', bookController.getBooksOnSale);
  *         - personal
  *         - all
  *       description: Enum for book categories
+ *     BookResponse:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - author
+ *         - description
+ *         - price
+ *         - categoryName
+ *       properties:
+ *         id:
+ *           type: number
+ *           example: 2
+ *         name:
+ *           type: string
+ *           example: "The Great Gatsby"
+ *         author:
+ *           type: string
+ *           example: "F. Scott Fitzgerald"
+ *         image:
+ *           type: string
+ *           nullable: true
+ *           example: "https://example.com/image.jpg"
+ *         description:
+ *           type: string
+ *           example: "A classic novel set in the 1920s."
+ *         price:
+ *           type: number
+ *           example: 15.99
+ *         discount:
+ *           type: number
+ *           example: 2.5
+ *         categoryName:
+ *           $ref: '#/components/schemas/BookTypeEnum'
+ *           example: "fiction"
+ *         userId:
+ *           type: number
+ *           example: 3
+ * /api/book/all:
+ *   get:
+ *     summary: Get all books
+ *     description: Fetches books from the database.
+ *     responses:
+ *       200:
+ *         description: A list of books.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/BookResponse'
+ */
+router.get('/all', bookController.getAllBooks);
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     BookTypeEnum:
+ *       type: string
+ *       enum:
+ *         - fiction
+ *         - romance
+ *         - children
+ *         - fantasy
+ *         - mystery
+ *         - business
+ *         - personal
+ *         - all
+ *       description: Enum for book categories
  *     BookDetailsResponse:
  *       type: object
  *       required:
