a4c0878f235c333c948f195073d6a3013e395c92
anaastastasiia <nastiaproturenko5@gmail.com>
2025-05-11 21:39:35 +0200

    bugfix

--- front-end/src/pages/LibraryPage.tsx
+++ front-end/src/pages/LibraryPage.tsx
@@ -1,14 +1,20 @@
-import { booksStore } from '../store/useBooks';
+import { booksActions, booksStore } from '../store/useBooks';
 import BookItem from '../components/Book/Book';
 import { BookFilters } from '../components/BookFilters/BookFilters';
+import { useEffect } from 'react';
 
 export const LibraryPage = () => {
-    const { booksOnSale } = booksStore();
+    const { books } = booksStore();
+    const { getAllBooks } = booksActions;
+
+    useEffect(() => {
+        getAllBooks();
+    }, []);
 
     return (
         <div className="overflow-x-hidden">
             <div>
-                {booksOnSale.length ? (
+                {books.length ? (
                     <div className="container bg-[#f9f9f9]">
                         <div className="flex md:flex-row flex-col">
                             <div className="lg:w-1/4 p-4 w-full">
@@ -16,7 +22,7 @@ export const LibraryPage = () => {
                             </div>
                             <div className="lg:w-3/4 p-6 w-full">
                                 <div className="grid lg:grid-cols-4 md:grid-cols-2 space-y-6 md:space-y-0 gap-6">
-                                    {booksOnSale.map((item, index) => {
+                                    {books.map((item, index) => {
                                         return (
                                             <BookItem {...item} key={index} />
                                         );
