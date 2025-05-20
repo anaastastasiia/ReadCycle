767d49b355040ae8c5d70f411502f8d80e4a3561
anaastastasiia <nastiaproturenko5@gmail.com>
2025-05-20 21:48:21 +0200

    new button

--- front-end/src/pages/User/UserBooksPage.tsx
+++ front-end/src/pages/User/UserBooksPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { booksActions, booksStore } from '../../store/useBooks';
 import { BookCard } from '../../components/UserBook/BookCard';
@@ -7,6 +8,7 @@ import { BookDetails } from '../../model/types';
 
 export const UserBooksPage = () => {
     const { t } = useTranslation();
+    const navigate = useNavigate();
     const { user } = authStore();
     const { userSellBooks, shouldRefresh } = booksStore();
     const { getBooksForUser, setShouldRefresh } = booksActions;
@@ -33,11 +35,20 @@ export const UserBooksPage = () => {
         setLocalBooks(userSellBooks);
     }, [userSellBooks]);
 
+    const createNewBook = () => {
+        navigate('/create');
+    };
+
     return (
         <div className="overflow-x-hidden flex container flex-col my-8">
-            <h1 className="text-2xl font-bold mb-4">
-                {t('pages:userPage.sell.title')}
-            </h1>
+            <div className="flex justify-between">
+                <h1 className="text-2xl font-bold mb-4">
+                    {t('pages:userPage.sell.title')}
+                </h1>
+                <button className="primary-btn" onClick={createNewBook}>
+                    {t('pages:authPage.accountMenu.createItem')}
+                </button>
+            </div>
 
             <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-4">
                 {localBooks.map((book) => (
