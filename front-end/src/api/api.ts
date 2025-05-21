415ef2c873d8ce0fd02fdd7590403c34f44953a2
anaastastasiia <nastiaproturenko5@gmail.com>
2025-05-21 20:23:52 +0200

    generate api

--- front-end/src/api/api.ts
+++ front-end/src/api/api.ts
@@ -592,6 +592,31 @@ export interface UserBooksResponse {
      */
     'currentPage'?: number;
 }
+/**
+ * 
+ * @export
+ * @interface UserNameResponse
+ */
+export interface UserNameResponse {
+    /**
+     * 
+     * @type {number}
+     * @memberof UserNameResponse
+     */
+    'id': number;
+    /**
+     * 
+     * @type {string}
+     * @memberof UserNameResponse
+     */
+    'firstName': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof UserNameResponse
+     */
+    'lastName': string;
+}
 
 /**
  * AuthenticationApi - axios parameter creator
@@ -780,6 +805,40 @@ export class AuthenticationApi extends BaseAPI {
  */
 export const DefaultApiAxiosParamCreator = function (configuration?: Configuration) {
     return {
+        /**
+         * Fetches user\'s name by id from the database.
+         * @summary Get username by id
+         * @param {number} id ID of the book
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        apiAuthUserIdGet: async (id: number, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'id' is not null or undefined
+            assertParamExists('apiAuthUserIdGet', 'id', id)
+            const localVarPath = `/api/auth/user/{id}`
+                .replace(`{${"id"}}`, encodeURIComponent(String(id)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
         /**
          * Fetches books from the database.
          * @summary Get all books
@@ -1097,6 +1156,19 @@ export const DefaultApiFp = function(configuration?: Configuration) {
     const localVarAxiosParamCreator = DefaultApiAxiosParamCreator(configuration)
     return {
         /**
+         * Fetches user\'s name by id from the database.
+         * @summary Get username by id
+         * @param {number} id ID of the book
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async apiAuthUserIdGet(id: number, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<UserNameResponse>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.apiAuthUserIdGet(id, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['DefaultApi.apiAuthUserIdGet']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
          * Fetches books from the database.
          * @summary Get all books
          * @param {*} [options] Override http request option.
@@ -1216,6 +1288,16 @@ export const DefaultApiFactory = function (configuration?: Configuration, basePa
     const localVarFp = DefaultApiFp(configuration)
     return {
         /**
+         * Fetches user\'s name by id from the database.
+         * @summary Get username by id
+         * @param {number} id ID of the book
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        apiAuthUserIdGet(id: number, options?: RawAxiosRequestConfig): AxiosPromise<UserNameResponse> {
+            return localVarFp.apiAuthUserIdGet(id, options).then((request) => request(axios, basePath));
+        },
+        /**
          * Fetches books from the database.
          * @summary Get all books
          * @param {*} [options] Override http request option.
@@ -1310,6 +1392,18 @@ export const DefaultApiFactory = function (configuration?: Configuration, basePa
  * @extends {BaseAPI}
  */
 export class DefaultApi extends BaseAPI {
+    /**
+     * Fetches user\'s name by id from the database.
+     * @summary Get username by id
+     * @param {number} id ID of the book
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof DefaultApi
+     */
+    public apiAuthUserIdGet(id: number, options?: RawAxiosRequestConfig) {
+        return DefaultApiFp(this.configuration).apiAuthUserIdGet(id, options).then((request) => request(this.axios, this.basePath));
+    }
+
     /**
      * Fetches books from the database.
      * @summary Get all books
